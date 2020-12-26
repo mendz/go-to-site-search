aaea392440b19e6a18121a47ab1895910f2cf8ba
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2020-12-26 23:11:53 +0200

    update build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const AdmZip = require('adm-zip');
 
 const zip = new AdmZip();
@@ -5,6 +6,16 @@ const zip = new AdmZip();
 const files = ['manifest.json', 'popup.js', 'popup.html', 'popup-styles.css'];
 const folders = ['./icons'];
 const destZipPath = './build/go-to-site-search.zip';
+
+if (fs.existsSync(destZipPath)) {
+  console.log(`
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              BUILD CLEANING
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+`);
+  fs.unlinkSync(destZipPath);
+  console.log(`'${destZipPath}' was deleted`);
+}
 
 console.log(`
   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -25,7 +36,7 @@ console.log(`
 
 folders.forEach((folder) => {
   console.log(`'${folder}'`);
-  zip.addLocalFolder(folder);
+  zip.addLocalFolder(folder, 'icons');
 });
 
 zip.writeZip(destZipPath, () =>
