36f7a3cf0346ae6f933e5beb5cd2e41451f930b3
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2020-12-26 21:21:55 +0200

    change multiple inputs to one domain and textarea of queries

--- popup.js
+++ popup.js
@@ -1,82 +1,35 @@
 /* global chrome */
-let counter = 0;
-const html = String.raw;
-
-function generateTemplate(index) {
-  return html`
-    <fieldset data-index="${index}">
-      <label for="domain-site">
-        Domain/Site (${index})
-        <input
-          type="text"
-          name="domain-site-${index}"
-          id="domain-site-${index}"
-          placeholder="Enter domain or site name"
-          required
-        />
-      </label>
-      <label for="query" class="input-wrapper">
-        Query (${index})
-        <input
-          type="text"
-          name="query-${index}"
-          id="query-${index}"
-          placeholder="Enter extra query (optional)"
-        />
-      </label>
-    </fieldset>
-  `;
+/**
+ * @param {string} site
+ * @param {string} query
+ */
+function getUrl(site, query) {
+  return `https://www.google.com/search?q=site:${site}${
+    query ? `%20${query}` : ''
+  }`;
 }
 
-function checkButtonsStatus() {
-  if (counter === 0) {
-    document.querySelector('#remove-inputs').disabled = true;
-    document.querySelector('button[type="submit"]').disabled = true;
-  } else {
-    document.querySelector('#remove-inputs').disabled = false;
-    document.querySelector('button[type="submit"]').disabled = false;
-  }
-}
-
-function addInputs() {
-  const wrapper = document.querySelector('.input-wrapper');
-  let htmlString = '';
-  counter += 1;
-  htmlString += generateTemplate(counter);
-  wrapper.insertAdjacentHTML('beforeend', htmlString);
-  checkButtonsStatus();
-}
-
-function minusInputs() {
-  const wrapper = document.querySelector('.input-wrapper');
-  const inputsToRemove = wrapper.querySelector(
-    `fieldset[data-index="${counter}"]`
-  );
-  counter -= 1;
-  wrapper.removeChild(inputsToRemove);
-  checkButtonsStatus();
-}
-
-function search(event) {
-  event.preventDefault();
-  Array.from({ length: counter }).forEach((_, i) => {
-    const site = event.target.elements[`domain-site-${i + 1}`].value;
-    const query = event.target.elements[`query-${i + 1}`].value;
+/**
+ * @param {string} site
+ * @param {string[]} queries
+ */
+function openTabs(site, queries) {
+  queries.forEach((query) => {
     chrome.tabs.create({
-      url: `https://www.google.com/search?q=site:${site}${
-        query ? `%20${query}` : ''
-      }`,
+      url: getUrl(site, query),
       active: true,
     });
   });
 }
 
+function search(event) {
+  event.preventDefault();
+  const site = event.target.elements.domainSite.value;
+  const queriesString = event.target.elements.queries.value?.trim() ?? '';
+  const queries = queriesString.length ? queriesString.split('\n') : [''];
+  openTabs(site, queries);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
-  form.addEventListener('submit', search);
-  addInputs();
-  document.querySelector('#add-inputs').addEventListener('click', addInputs);
-  document
-    .querySelector('#remove-inputs')
-    .addEventListener('click', minusInputs);
+  document.querySelector('form').addEventListener('submit', search);
 });
