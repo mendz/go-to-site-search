88a45677cde7f9262ece1de9b1376b3ac78d8f6b
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2020-12-26 21:30:37 +0200

    Add option to use multiply queries (#1)

--- popup.js
+++ popup.js
@@ -1,16 +1,35 @@
 /* global chrome */
+/**
+ * @param {string} site
+ * @param {string} query
+ */
+function getUrl(site, query) {
+  return `https://www.google.com/search?q=site:${site}${
+    query ? `%20${query}` : ''
+  }`;
+}
+
+/**
+ * @param {string} site
+ * @param {string[]} queries
+ */
+function openTabs(site, queries) {
+  queries.forEach((query) => {
+    chrome.tabs.create({
+      url: getUrl(site, query),
+      active: true,
+    });
+  });
+}
 
 function search(event) {
   event.preventDefault();
   const site = event.target.elements.domainSite.value;
-  const query = event.target.elements.query.value;
-  chrome.tabs.create({
-    url: `https://www.google.com/search?q=site:${site}${!!query ? '%20'+query : ''}`,
-    active: true,
-  });
+  const queriesString = event.target.elements.queries.value?.trim() ?? '';
+  const queries = queriesString.length ? queriesString.split('\n') : [''];
+  openTabs(site, queries);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
-  form.addEventListener('submit', search);
+  document.querySelector('form').addEventListener('submit', search);
 });
