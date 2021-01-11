44a03506d510acfddeeec34cb94122de460e42b3
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2021-01-11 02:09:46 +0200

    #6 Save&Load inputs' value

--- src/popup.js
+++ src/popup.js
@@ -56,9 +56,59 @@ function handleDomainChange(event) {
   }
 }
 
+/**
+ * @param {InputEvent} event
+ */
+function handleSaveInputValues(event) {
+  let { value, dataset } = event.target;
+  if (dataset.name === 'domain') {
+    value = trimDomain(value);
+  }
+  chrome.storage.sync.set({ [event.target.dataset.name]: value }, () => {
+    const error = chrome.runtime.lastError;
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(`saved - (${dataset.name}) with: (${value})`);
+    }
+  });
+}
+
+function clearInputs() {
+  document.querySelector('#domainSite').value = '';
+  document.querySelector('#queries').value = '';
+  chrome.storage.sync.clear(() => {
+    const error = chrome.runtime.lastError;
+    if (error) {
+      console.error(error);
+    }
+  });
+}
+
+function saveInputsValue() {
+  // save inputs on chrome storage
+  document
+    .querySelector('#domainSite')
+    .addEventListener('input', handleSaveInputValues);
+  document
+    .querySelector('#queries')
+    .addEventListener('input', handleSaveInputValues);
+  chrome.storage.sync.get(['domain', 'queries'], (result) => {
+    const error = chrome.runtime.lastError;
+    if (error) {
+      console.error(error);
+      return;
+    }
+    document.querySelector('#domainSite').value = result.domain ?? '';
+    document.querySelector('#queries').value = result.queries ?? '';
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('form').addEventListener('submit', search);
+  document.querySelector('button#clear').addEventListener('click', clearInputs);
   document
     .querySelector('#domainSite')
     .addEventListener('input', handleDomainChange);
+  saveInputsValue();
 });
